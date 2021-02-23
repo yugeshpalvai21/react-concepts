5ff6bbc38089595af37cd2d4c4f478aec92d6208
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-02-23 09:05:00 +0530

    adds react, react-dom and webpack dependencies and configures webpack.config.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+console.log('js loaded from webpack successfully')
+
+console.log(React);
+
+console.log(ReactDOM);
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public', 'js')
+    }
+}
