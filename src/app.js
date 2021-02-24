58dfa16838c792a10be0213ab10fe5f02c75bb34
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-02-24 09:07:49 +0530

    Implements Counter Class Component with Default State

--- src/app.js
+++ src/app.js
@@ -4,7 +4,24 @@ import ReactDOM from 'react-dom';
 
 //const jsx = React.createElement('h1', null, 'Welcome Aborad, This Is React');
 
-const jsx = <h1>Welcome Aborad, This Is From React</h1>;
+//const jsx = <h1>Welcome Aborad, This Is From React</h1>;
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h3>Counter App - React</h3>
+                <h4>Counter - {this.state.counter}</h4>
+            </div>
+        )
+    }
+}
 const domElement = document.getElementById('app');
 
-ReactDOM.render(jsx, domElement);
+ReactDOM.render(<Counter />, domElement);
