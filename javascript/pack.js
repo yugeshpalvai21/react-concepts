c1f6f7f1e069b46446dae9aa281ab058c41a9b44
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-23 09:36:57 +0530

    basic \form submission with React

--- javascript/pack.js
+++ javascript/pack.js
@@ -1,5 +1,3 @@
-const jsx = <h1>Let's Play Again</h1>
-
 const element = document.getElementById('app')
 
 ReactDOM.render(jsx, element);
--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 "use strict";
 
-var jsx = /*#__PURE__*/React.createElement("h1", null, "Let's Play Again");
 var element = document.getElementById('app');
 ReactDOM.render(jsx, element);
