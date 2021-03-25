48535c707105be067e2fa05017926a4393e98303
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-25 09:24:22 +0530

    React Counter Example Without defining class or functional component

--- javascript/pack.js
+++ javascript/pack.js
@@ -1,3 +1,41 @@
-const element = document.getElementById('app')
 
-ReactDOM.render(jsx, element);
+let counter = 0;
+
+const handleAddOne = () => {
+    // console.log('AddOne Button Clicked')
+    counter += 1;
+    console.log(counter);
+    render();
+}
+
+const handleMinusOne = () => {
+    // console.log('MinusOne Button Clicked')
+    counter -= 1;
+    console.log(counter);
+    render();
+}
+
+const handleReset = () => {
+    // console.log('Reset Button Clicked')
+    counter = 0;
+    console.log(counter);
+    render();
+}
+
+const render = () => {
+    const jsx = (
+        <div>
+            <h3>Counter: {counter}</h3> 
+            {/* React.CreateElement("h3", null, "Conter: ", counter) */}
+            <button onClick={handleAddOne}>AddOne(+1)</button>
+            <button onClick={handleMinusOne}>MinusOne(-1)</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    );
+
+    const domElement = document.getElementById('app');
+
+    ReactDOM.render(jsx, domElement);
+}
+
+render();
--- public/app.js
+++ public/app.js
@@ -1,4 +1,38 @@
 "use strict";
 
-var element = document.getElementById('app');
-ReactDOM.render(jsx, element);
+var counter = 0;
+
+var handleAddOne = function handleAddOne() {
+  // console.log('AddOne Button Clicked')
+  counter += 1;
+  console.log(counter);
+  render();
+};
+
+var handleMinusOne = function handleMinusOne() {
+  // console.log('MinusOne Button Clicked')
+  counter -= 1;
+  console.log(counter);
+  render();
+};
+
+var handleReset = function handleReset() {
+  // console.log('Reset Button Clicked')
+  counter = 0;
+  console.log(counter);
+  render();
+};
+
+var render = function render() {
+  var jsx = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h3", null, "Counter: ", counter), /*#__PURE__*/React.createElement("button", {
+    onClick: handleAddOne
+  }, "AddOne(+1)"), /*#__PURE__*/React.createElement("button", {
+    onClick: handleMinusOne
+  }, "MinusOne(-1)"), /*#__PURE__*/React.createElement("button", {
+    onClick: handleReset
+  }, "Reset"));
+  var domElement = document.getElementById('app');
+  ReactDOM.render(jsx, domElement);
+};
+
+render();
