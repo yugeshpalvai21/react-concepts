3304ac31f7f13ed594dedd2bed5140b659cf009e
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-23 08:01:12 +0530

    Implements Router Switch to handle exact match of URL

--- react_router/app.js
+++ react_router/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const Home = () => (
         <h1>Home Page.. will Update Soon...</h1>
@@ -18,12 +18,19 @@ const Contact = () => (
     <h5>This Section Displays Contact Details....</h5>
 )
 
+const NotFound = () => (
+    <h3>Pages That You're Trying to Access Is Not Available</h3>
+)
+
 const jsx = (
     <BrowserRouter>
-        <Route path="/" component={Home} exact={true}/>
-        <Route path="/about" component={About} />
-        <Route path="/portfolios" component={Portfolios} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+            <Route path="/" component={Home} exact={true}/>
+            <Route path="/about" component={About} />
+            <Route path="/portfolios" component={Portfolios} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound} />
+        </Switch>
     </BrowserRouter>
 );
 
