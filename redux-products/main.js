ca0d14729c3b7b276b32c329ae52303e7a911c79
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-25 09:38:46 +0530

    Adds default case to return state for reducers

--- redux-products/main.js
+++ redux-products/main.js
@@ -1,10 +1,17 @@
 import { createStore, combineReducers } from 'redux';
 
+// PRODUCTS REDUCERRR
+
 const productsReducerDefaultData = [{id: '123', title: 'some product', price: '123', created_at: '123'}];
 
-const productsReducer = (state = productsReducerDefaultData) => {
-    return state;
+const productsReducer = (state = productsReducerDefaultData,action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
 }
+
+// FILTERS REDUCER
 
 const filtersReducerDefaultData = {
     text: "",
@@ -13,11 +20,20 @@ const filtersReducerDefaultData = {
     endDate: "123"
 }
 
-const filtersReducer = (state = filtersReducerDefaultData) => {
-    return state;
+const filtersReducer = (state = filtersReducerDefaultData, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
 }
+
+// COMBINING REDUCERS
 
 const store = createStore(combineReducers({products: productsReducer, filters: filtersReducer}));
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({type: 'ADD_PRODUCT'});
 
