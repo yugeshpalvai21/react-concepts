d091be39bf12e245abf9431497b24baa0a7cd789
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-05 09:57:02 +0530

    Adds Basic Routes - Home, About and Contact

--- expensify/main.js
+++ expensify/main.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const domElement = document.getElementById('app');
 
 const Header = () => (
     <div>
-        <h1>Main Title</h1>
-        <p>main paragraph</p>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
     </div>
 )
 
+const About = () => <h3>This is About Page</h3>;
+
+const Contact = () => <h3>This is Contact Page</h3>;
+
+const PageNotFound = () => <h3>This Page Is Not Defined...</h3>;
+
 const jsx = (
     <BrowserRouter>
-        <Route path="/" component={Header} />
+        <Header />
+        <Route path="/" component={Header} exact={true}/>
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={PageNotFound} />
     </BrowserRouter>
 );
 
