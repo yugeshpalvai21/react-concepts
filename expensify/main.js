45a6bec5bdc1acb5e9bb2affc222cfa29999e14b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-12 08:33:48 +0530

    Implements Header Component for Expensify

--- expensify/main.js
+++ expensify/main.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This Is Heading</h1>
+            <p>This is Paragraph from Header Component Renderrr</p>
+        </div>
+    )
+}
 
 const domElement = document.getElementById('app');
 
-const Header = () => (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-    </div>
-)
-
-const About = () => <h3>This is About Page</h3>;
-
-const Contact = () => <h3>This is Contact Page</h3>;
-
-const PageNotFound = () => <h3>This Page Is Not Defined...</h3>;
-
-const jsx = (
-    <BrowserRouter>
-        <Header />
-        <Route path="/" component={Header} exact={true}/>
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route component={PageNotFound} />
-    </BrowserRouter>
-);
+ReactDOM.render(<Header />, domElement);
 
 
-ReactDOM.render(jsx, domElement);
+// console.log(typeof React);
+// console.log(typeof ReactDOM);
+
+// console.dir(React);
+
