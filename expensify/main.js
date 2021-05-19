23714d37b2768c9853dd476672bd15733be4286b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-19 08:45:30 +0530

    installs react-redux module

--- expensify/main.js
+++ expensify/main.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 import { configureStore } from './store/configureStore.js';
-import { AddExpense } from './actions/expenses.js';
-import { visibleExpenses } from './selectors/visibleExpenses.js'; 
-
 import { jsx } from './components/AppRouter.js';
-
-const domElement = document.getElementById('app');
-
-ReactDOM.render(jsx, domElement);
 
 const store = configureStore();
 
-console.log(store.getState());
+const domElement = document.getElementById('app');
+ReactDOM.render(jsx, domElement);
 
-store.dispatch(AddExpense({ description: 'new expense adding', price: 99.99}));
 
-console.log(store.getState());
 
 
