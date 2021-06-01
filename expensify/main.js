1840cebca25e47b34a9f4cd6e1faafbdb344db6b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-06-01 15:17:25 +0530

    imports and implements Provider Component from react-redux

--- expensify/main.js
+++ expensify/main.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './components/AppRouter.js';
+import { Provider } from 'react-redux';
 
 import { configureStore } from './store/configureStore.js';
 import { AddExpense } from './actions/expenses.js';
 
-const domElement = document.getElementById('app');
-ReactDOM.render(<AppRouter />, domElement);
-
 const store = configureStore();
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+const domElement = document.getElementById('app');
+ReactDOM.render(jsx, domElement);
+
 
 console.log(store);
 console.log(store.getState());
