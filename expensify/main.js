e46ebb9c0ad609d654bb41917bad3cbf8016acea
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-06-01 09:42:42 +0530

    access redux store object from react main access point

--- expensify/main.js
+++ expensify/main.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './components/AppRouter.js';
 
+import { configureStore } from './store/configureStore.js';
+import { AddExpense } from './actions/expenses.js';
+
 const domElement = document.getElementById('app');
 ReactDOM.render(<AppRouter />, domElement);
+
+const store = configureStore();
+
+console.log(store);
+console.log(store.getState());
+store.dispatch(AddExpense());
+console.log(store.getState());
 
 
 
