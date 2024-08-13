5e106bcda2d712d320519f26bbfe87963a0880f1
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-13 19:01:37 +0530

    chore: moved Main to App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.main}>
+      <h1>Welcome Message</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Main from './Main.js';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
 
