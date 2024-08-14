d48364580658085066c616af62907c05f3485d5d
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-14 23:55:07 +0530

    implementing counter app using useState hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import styles from './App.module.css';
-
-function Person(props) {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      { props.isMajor && <h3>you're eligible to drive</h3> }
-    </div>
-  );
-}
+import { Counter } from './components/Counter';
 
 function App() {
   return (
     <div>
-      <Person name="Yugesh" age={25} isMajor={true} />
-      <Person name="Palvai" age={25} isMajor={false}/>
+      <Counter />
     </div>
   );
 }
