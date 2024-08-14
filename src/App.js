defc5f7f079739827b70583efb5e9db04e088b91
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-14 22:54:35 +0530

    revisits component, jsx, props, and coditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import styles from './App.module.css';
 
+function Person(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      { props.isMajor && <h3>you're eligible to drive</h3> }
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className={styles.main}>
-      <h1>Welcome Message</h1>
+    <div>
+      <Person name="Yugesh" age={25} isMajor={true} />
+      <Person name="Palvai" age={25} isMajor={false}/>
     </div>
   );
 }
